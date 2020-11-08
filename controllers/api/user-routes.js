754f4dcabb5f29ba8fc16770edd633e74189cf0f
B. Drawe <brycerdrawe.b@gmail.com>
2020-11-08 16:58:56 -0700

    heroku stuff

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
 const { User } = require('../../models');
-const { restore } = require('../../models/User');
+// const { restore } = require('../../models/User');
 
 // GET /api/users
 router.get('/', (req, res) => {
