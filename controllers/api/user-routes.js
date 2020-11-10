c78119c58597c2964155d0299f4b01f24268512f
Aztec <salgorog@hotmail.com>
2020-11-09 20:00:57 -0700

    working on creating a session on the back end

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -42,41 +42,61 @@ router.post('/', (req, res) => {
         password: req.body.password
     })
 
-    .then(dbUserData =>{
-        res.session.save(() => {
-            req.session.user_id = dbUserData.id,
+        .then(dbUserData => {
+            res.session.save(() => {
+                req.session.user_id = dbUserData.id,
+                    req.session.firstname = dbUserData.firstname,
+                    req.session.lastname = dbUserData.lastname,
+                    req.ression.loggedIn = true
+
+                res.json(dbUserData);
+            });
+        })
+        .catch(err => {
+            console.log(err)
+            res.status(500).json(err)
+        })
+});
+
+router.post('/login', (req, res) => {
+    // expects {email: 'sample@gmail.com', password: 'password1234'}
+    User.findOne({
+        where: {
+            email: req.body.email
+        }
+    }).then(dbUserData => {
+        if (!dbUserData) {
+            res.status(400).json({ message: 'No user with that email address!' });
+            return;
+        }
+
+        const validPassword = dbUserData.checkPassword(req.body.password);
+        if (!validPassword) {
+            res.status(400).json({ message: 'Incorrect password!' });
+            return;
+        }
+
+        req.session.save(() => {
+            // declare session variables
+            req.session.user_id = dbUserData.id;
             req.session.firstname = dbUserData.firstname,
-            req.session.lastname = dbUserData.lastname,
-            req.ression.loggedIn = true
+                req.session.lastname = dbUserData.lastname,
+                req.session.loggedIn = true;
 
-            res.json(dbUserData);
+            res.json({ user: dbUserData, message: 'You are now logged in!' });
         });
-    })
-    .catch( err => {
-        console.log(err)
-        res.status(500).json(err)
-    })
+    });
 });
-router.post('/login', (req, res)=>{
-  // expects {email: 'sample@gmail.com', password: 'password1234'}
-  User.findOne({
-    where: {
-        email: req.body.email
-    }
-}).then(dbUserData => {
-    if (!dbUserData) {
-        res.status(400).json({ message: 'No user with that email address!' });
-        return;
-    }
 
-    const validPassword = dbUserData.checkPassword(req.body.password);
-    if (!validPassword) {
-        res.status(400).json({ message: 'Incorrect password!' });
-        return;
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
     }
-
-    res.json({ user: dbUserData, message: 'You are now logged in!' });
-});
+    else {
+        res.status(404).end();
+    }
 });
 
 // PUT /api/users/1
